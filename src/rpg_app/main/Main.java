15d1b648eb459176904be9e87c9d4377e717b090
nakajimakazuya <nakajimakazuya@localhost>
2021-09-07 17:44:52 +0900

    first commit

---
+++ src/rpg_app/main/Main.java
@@ -0,0 +1,21 @@
+package rpg_app.main;
+import rpg_app.thief.Thief;
+
+public class Main {
+	public static void heal(int hp) {
+		hp += 10;
+	}
+	public static void heal(Thief thief) {
+		thief.hp += 10;
+	}
+	public static void main(String[] args) {
+		int baseHp = 25;
+		Thief t = new Thief("ハットリ", baseHp);
+		System.out.println(baseHp + ":" + t.hp);
+		heal(baseHp);
+		heal(t);
+		System.out.println(baseHp + ":" + t.hp);
+	}
+}
+
+
